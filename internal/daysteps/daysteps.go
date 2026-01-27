832cfcb967fc6c6db377cc133656b98fe9e7beb1
Artem Zaporozhets <mr.zaporozhec@yandex.ru>
2026-01-27 21:16:27 +0300

    Refactor fitness-tracker parsers and function for calculation spent calories, distance etc.
    daysteps.go:
    
    -Rewrite parsePackage and DayActionInfo
    
    spentcalories.go:
    
    - Update all calculation functions
    
    - parseTraining() now validates input format
    
    - distance(), meanSpeed() now calculates correctly
    
    - update TrainingInfo() output format

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,7 +1,14 @@
 package daysteps
 
 import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 const (
@@ -12,9 +19,53 @@ const (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	str := strings.Split(data, ",")
+	if len(str) != 2 {
+		return 0, 0, errors.New("invalid data")
+	}
+
+	steps, err := strconv.Atoi(str[0])
+	if err != nil {
+		return 0, 0, errors.New("invalid data, the first substring should have int")
+	}
+
+	if steps <= 0 {
+		return 0, 0, errors.New("invalid data, the count of steps should be positive")
+	}
+
+	walkDuration, err := time.ParseDuration(str[1])
+	if err != nil {
+		return 0, 0, errors.New("invalid data, the second substring should have time.Duration")
+	}
+
+	if walkDuration.Seconds() <= 0 {
+		return 0, 0, errors.New("invalid data, the duration should be positive")
+	}
+
+	return steps, walkDuration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+	steps, duration, err := parsePackage(data)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+
+	if steps <= 0 {
+		log.Println(err)
+		return ""
+	}
+
+	distance := float64(steps) * stepLength / float64(mInKm)
+
+	spentCalories, err := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+
+	return fmt.Sprintf("Количество шагов: %d.\n"+
+		"Дистанция составила %.2f км.\n"+
+		"Вы сожгли %.2f ккал.\n", steps, distance, spentCalories)
 }
